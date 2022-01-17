035b23abf380bc2f5d4385081dfd4479bd8002f7
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-17 22:19:49 +0900

    :cop:

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test 'should get show' do
     get top_show_url
     assert_response :success
   end
