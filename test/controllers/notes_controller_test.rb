134f5387e487f58915446a19eea71281e5d02f07
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-18 23:38:19 +0900

    Pass controller tests

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get notes_index_url
+    get notes_url
     assert_response :success
   end
 
   test 'should get show' do
-    get notes_show_url
+    get note_url(number: 'vNe5i7tmS5nwjliJ')
     assert_response :success
   end
 end
