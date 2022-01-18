3a6af8e0fe3ec5c9f1eea5d3709fa3c747b96891
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-18 23:01:14 +0900

    $ bin/rails g model Note number:string title:string content:text published_at:datetime

---
+++ db/migrate/20220118140107_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notes do |t|
+      t.string :number
+      t.string :title
+      t.text :content
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
