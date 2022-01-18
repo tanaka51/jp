3b9ba82436f917a87d41e924796be76b3c58c8eb
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-19 00:10:19 +0900

    Pass test

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
-    get top_show_url
+    get root_url
     assert_response :success
   end
 end
