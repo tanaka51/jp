592622afb02b2ea84ffd46b23e8178576a733fe9
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-19 23:06:15 +0900

    List 3 random notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class NotesController < ApplicationController
-  def index; end
+  def index
+    @notes = Note.first(3)
+  end
 
   def show
     @note = Note.published.find_by!(number: params[:number])
