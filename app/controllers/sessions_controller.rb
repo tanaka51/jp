490558cec6c4a7ae21b4e4a54ef63178229fd069
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-19 22:05:54 +0900

    Implement login/logut feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @session_form = SessionForm.new
+  end
 
-  def create; end
+  def create
+    @session_form = SessionForm.new(session_form_params)
+    if @session_form.authenticate
+      session[:logged_in] = true
 
-  def destroy; end
+      redirect_to notes_path, notice: 'ログインした'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:logged_in] = nil
+
+    redirect_to root_path
+  end
+
+  private
+
+  def session_form_params
+    params.require(:session_form).permit(:login_id, :password)
+  end
 end
