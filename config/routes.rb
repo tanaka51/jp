c4cdb6fc74b2cd1d9b6a3ca525c8784e23ae6759
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-19 21:01:33 +0900

    Tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root 'top#show'
+
+  get 'login', to: 'sessions#new'
+  post 'sessions', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :notes, path: 'n', param: 'number', only: %i[index show]
 end
