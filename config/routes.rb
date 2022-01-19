6b2fab7d2a2cd0205964e39c861c52096041a4c6
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-19 20:57:16 +0900

    $ bin/rails g controller Sessions new create destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'top#show'
 
   resources :notes, path: 'n', param: 'number', only: %i[index show]
