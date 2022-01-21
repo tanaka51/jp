9a304006a51bcf8bf3f6690475f5a205993589b3
Koichi Tanaka <tanaka51.jp@gmail.com>
2022-01-21 20:55:35 +0900

    :cop:

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+pin 'application', preload: true
+pin 'trix'
+pin '@rails/actiontext', to: 'actiontext.js'
